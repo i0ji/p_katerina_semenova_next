f8b6ecbe4115e222785d84417b3347d6a7148566
i0ji <ioji@inbox.ru>
2025-03-20 23:10:53 +0300

    tag

--- src/services/declarations.d.ts
+++ src/services/declarations.d.ts
@@ -1,27 +1,3 @@
-//LATER
-//   //FIXME
-// declare global {
-//   namespace SlideModelNamespace {
-//     interface SlideModel {
-//       id: number;
-//       img?: string;
-//     }
-
-//     interface SlidesDataModel {
-//       slides: Array<SlideModel>;
-//       description: string;
-//     }
-//   }
-
-//   // namespace AccordionModelNamespace {
-//   //   interface AccordionModel {
-//   //     description: string;
-//   //     plot: string;
-//   //     visibility: boolean;
-//   //   }
-//   // }
-// }
-
 interface SlideModel {
   id: number;
   img: string;
