bb8b7bd4288c958f7602ac0e49e25cf3f0b81be0
Ivan Vasilevich <ioji@inbox.ru>
2025-03-24 16:25:18 +0300

    feature: new embla slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import { v4 as uuidv4 } from 'uuid';
 
 export default function Home() {
   //CONSOLE
-  console.log('v: 0.3.3.4/24.03.25');
+  console.log('v: 0.3.3.4a/24.03.25');
 
   //OPTION
-  const isTested = 0;
+  const isTested = 1;
 
   return (
     <>
--- src/components/Slides/Slides.tsx
+++ src/components/Slides/Slides.tsx
@@ -1,25 +1,16 @@
 'use client';
 
 import { useRef, useState, useEffect } from 'react';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-import {
-  Navigation,
-  Autoplay,
-  Pagination,
-} from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-import Image from 'next/image';
 import Skeleton from 'react-loading-skeleton';
+//CURRENT
+import useEmblaCarousel from 'embla-carousel-react';
 
 import styles from './Slides.module.scss';
 
 export default function Slides(props: SlidesDataModel) {
   const [isLoaded, setIsLoaded] = useState(false);
   const imageRef = useRef<HTMLImageElement>(null);
+  const [emblaRef] = useEmblaCarousel();
   const navigationPrevRef = useRef<HTMLButtonElement>(null);
   const navigationNextRef = useRef<HTMLButtonElement>(null);
 
@@ -31,93 +22,35 @@ export default function Slides(props: SlidesDataModel) {
     };
   }, [props.slides]);
 
-
-  //CURRENT
-  //PAGINATION
-  const paginationStyle = styles.pagination;
-  const paginationStyleBullet = styles.bullet;
-  const paginationStyleBulletActive = styles.bulletActive;
-
-  //OPTION
-  // const sliderParams = {
-  //   modules: [Navigation, Autoplay, Pagination],
-  //   loop: true,
-  //   spaceBetween: 0,
-  //   slidesPerView: 1,
-  //   autoplay: {
-  //     delay: 4000,
-  //     pauseOnMouseEnter: true,
-  //   },
-  //   pagination: {
-  //     clickable: true,
-  //   },
-  //   navigation: {
-  //     prevEl: navigationPrevRef.current,
-  //     nextEl: navigationNextRef.current,
-  //   },
-  //   onBeforeInit: (swiper) => {
-  //     if (typeof swiper.params.navigation !== 'boolean') {
-  //       swiper.params.navigation!.prevEl =
-  //         navigationPrevRef.current;
-  //       swiper.params.navigation!.nextEl =
-  //         navigationNextRef.current;
-  //     }
-  //   },
-  // };
-
   return (
     <section className={styles.slides}>
       <div className={styles.slide}>
-        <Swiper
-          modules={[Navigation, Autoplay, Pagination]}
-          spaceBetween={0}
-          slidesPerView={1}
-          loop={true}
-          autoplay={{
-            delay: 4000,
-            pauseOnMouseEnter: true,
-          }}
-          navigation={{
-            prevEl: navigationPrevRef.current,
-            nextEl: navigationNextRef.current,
-          }}
-          onBeforeInit={(swiper) => {
-            if (typeof swiper.params.navigation !== 'boolean') {
-              swiper.params.navigation!.prevEl =
-                navigationPrevRef.current;
-              swiper.params.navigation!.nextEl =
-                navigationNextRef.current;
-            }
-          }}
-          pagination={{
-            clickable: true,
-            // el: paginationStyle,
-            // bulletClass: paginationStyleBullet,
-            // bulletActiveClass: paginationStyleBulletActive,
-          }}
-        >
-          {props.slides.map((slide) => (
-            <SwiperSlide key={slide.id}>
-              {isLoaded ? (
-                <Image
-                  ref={imageRef}
-                  src={slide.img}
-                  alt={props.description}
-                  className={styles.slide__image}
-                  width={1600}
-                  height={900}
-                  loading="lazy"
-                />
-              ) : (
-                <Skeleton
-                  height={900}
-                  width={'100%'}
-                  style={{ borderRadius: 5 }}
-                />
-              )}
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        <div className="embla" ref={emblaRef}>
+          <div className="embla__container">
+            {props.slides.map((slide) => (
+              <div className="embla__slide">
+                {isLoaded ? (
+                  <img
+                    ref={imageRef}
+                    src={slide.img}
+                    alt={props.description}
+                    className={styles.slide__image}
+                    width={1600}
+                    height={900}
+                    loading="lazy"
+                  />
+                ) : (
+                  <Skeleton
+                    height={900}
+                    width={'100%'}
+                    style={{ borderRadius: 5 }}
+                  />
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+
         <button
           ref={navigationPrevRef}
           className={styles.slide__leftArrow}
