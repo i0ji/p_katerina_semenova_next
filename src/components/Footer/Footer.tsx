d78c98554e7689a22d838f3621ce2aa643156565
Ivan Vasilevich <ioji@inbox.ru>
2025-06-30 09:19:26 +0300

    issue#1 tg link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -73,7 +73,7 @@ export default function Footer() {
           </p>
           <p>
             <a href="https://t.me/KateSemenovaV">
-              KateSemenovaV
+              @KateSemenovaV
             </a>
           </p>
           <p>
