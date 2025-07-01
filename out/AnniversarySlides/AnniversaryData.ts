a7b83430e4d9631ef8ed060ab99ef26fc773fae0
Ivan Vasilevich <ioji@inbox.ru>
2025-07-01 10:55:01 +0300

    Remove out directory from git index

--- out/AnniversarySlides/AnniversaryData.ts
+++
@@ -1,8 +0,0 @@
-export const AnniversarySlides = [
-  { id: 1000, img: './AnniversarySlides/a1.webp' },
-  { id: 1001, img: './AnniversarySlides/a2.webp' },
-  { id: 1002, img: './AnniversarySlides/a3.webp' },
-  { id: 1003, img: './AnniversarySlides/a4.webp' }
-];
-
-export const AnniversaryDescription = `Бассейн «Чайка» — оформление к\u00A065-летию бассейна — 2022`;
