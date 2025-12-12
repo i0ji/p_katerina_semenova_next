4f362da82de6c388d86daaeb6245774bd44ba1bb
i0ji <ioji@inbox.ru>
2025-12-12 09:01:31 +0300

    docs: docs update
    МЕТА ДАННЫЕ: 0.4.5

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,14 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    reactStrictMode: true,
-    output: 'export',
-    images: {
-        unoptimized: true
-    },
-    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-    basePath: '',
-    assetPrefix: '.',
-    webpack: (config) => {
-        config.resolve.alias['@styles'] = './src/styles/*';
-        config.module.rules.push({
-            test: /\.(woff|woff2|eot|ttf|otf)$/,
-            use: {
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[hash].[ext]',
-                    outputPath: 'fonts/',
-                    publicPath: '/_next/static/fonts/'
-                }
-            }
-        });
-        return config;
-    }
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  basePath: '',
+  assetPrefix: '.',
 };
 
 export default nextConfig;
