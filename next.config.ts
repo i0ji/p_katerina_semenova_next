e2602ae00455026b1bec5b3db91b6c5c0389679e
i0ji <ioji@inbox.ru>
2025-12-12 08:54:07 +0300

    fix(maintenance): turbopack fix
    remove webpack  in favor of turbopack setup

--- next.config.ts
+++ next.config.ts
@@ -9,21 +9,21 @@ const nextConfig: NextConfig = {
     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
     basePath: '',
     assetPrefix: '.',
-    webpack: (config) => {
-        config.resolve.alias['@styles'] = './src/styles/*';
-        config.module.rules.push({
-            test: /\.(woff|woff2|eot|ttf|otf)$/,
-            use: {
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[hash].[ext]',
-                    outputPath: 'fonts/',
-                    publicPath: '/_next/static/fonts/'
-                }
-            }
-        });
-        return config;
-    }
+    // webpack: (config) => {
+    //     // config.resolve.alias['@styles'] = './src/styles/*';
+    //     config.module.rules.push({
+    //         test: /\.(woff|woff2|eot|ttf|otf)$/,
+    //         use: {
+    //             loader: 'file-loader',
+    //             options: {
+    //                 name: '[name].[hash].[ext]',
+    //                 outputPath: 'fonts/',
+    //                 publicPath: '/_next/static/fonts/'
+    //             }
+    //         }
+    //     });
+    //     return config;
+    // }
 };
 
 export default nextConfig;
